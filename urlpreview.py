26fb950d488687d5094f28819449abda30bd2cfa
fro_ozen <fro_ozen@gmx.de>
2015-07-24 15:02:27 +0200

    Add urlpreview plugin

---
+++ urlpreview.py
@@ -0,0 +1,29 @@
+from pybot import irc, plugin_manager
+import re
+import urllib
+
+def on_privmsg ( event, server ):
+    """Detect urls in messages and reply with their titles"""
+
+    channel = event.args [ 0 ]
+    message = event.args [ 1 ]
+
+    # Find the URLs
+    for url_match in re.finditer( "http(s)://.*?( |$)", message ):
+        url = url_match.group( 0 )
+
+        try:
+            html = urllib.urlopen( url ).read ()
+            # Find the title tag
+            title_match = re.search( "<title *>(.*?)</title *>", html )
+            if title_match:
+                # Reply with the URL's title
+                reply = irc.Irc_event ( "PRIVMSG", channel, "[%s] - %s" % ( title_match.group(1), url ) )
+                server.send_event ( reply )
+
+        # The opening of URLs might fail
+        except IOError:
+            reply = irc.Irc_event ( "PRIVMSG", channel, "Failed to open url: %s" % url )
+            server.send_event ( reply )
+
+plugin_manager.register_event_handler ( "PRIVMSG", on_privmsg )

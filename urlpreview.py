3c79f8c0b5b4c63694505ff5b9a80751d9f9855c
fro_ozen <fro_ozen@gmx.de>
2015-07-26 00:20:50 +0200

    Selectively preview URLs
    A primitive measure to prevent the bot from downloading large files, for
    example pdfs, that were linked on the channel

--- urlpreview.py
+++ urlpreview.py
@@ -3,6 +3,21 @@ import re
 import urllib
 import HTMLParser
 
+def valid_url ( url ):
+    """Check wether an URL should be highlighted"""
+
+    valid_filetypes = [ "html", "htm", "php" ]
+    url_split = url.split ( "/" ) [ 2: ]
+
+    # Highlight it, if it has no filetype or is the home page
+    if len ( url_split ) == 1 or not "." in url_split [ -1 ]:
+        return True
+
+    # Otherwise, check wether it has a valid filetype
+    else:
+        filetype = url_split [ -1 ].split ( "." ) [ -1 ]
+        return filetype in valid_filetypes
+
 def on_privmsg ( event, server ):
     """Detect urls in messages and reply with their titles"""
 
@@ -13,20 +28,22 @@ def on_privmsg ( event, server ):
     for url_match in re.finditer( "http(s)?://.*?( |$)", message ):
         url = url_match.group( 0 )
 
-        try:
-            html = urllib.urlopen( url ).read ()
-            # Find the title tag
-            title_match = re.search( "(?s)<title *>(.*?)</title *>", html )
-            if title_match:
-                # Reply with the URL's title
-                title = title_match.group( 1 ).strip ()
-                title = HTMLParser.HTMLParser().unescape( title )
-                reply = irc.Irc_event ( "PRIVMSG", channel, "[%s] - %s" % ( title, url ) )
-                server.send_event ( reply )
+        # Check wether the URL should be highlighted
+        if valid_url ( url ):
+            try:
+                html = urllib.urlopen( url ).read ()
+                # Find the title tag
+                title_match = re.search( "(?s)<title *>(.+?)</title *>", html )
+                if title_match:
+                    # Reply with the URL's title
+                    title = title_match.group( 1 ).strip ()
+                    title = HTMLParser.HTMLParser().unescape( title )
+                    reply = irc.Irc_event ( "PRIVMSG", channel, "[%s] - %s" % ( title, url ) )
+                    server.send_event ( reply )
 
-        # The opening of URLs might fail
-        except IOError:
-            reply = irc.Irc_event ( "PRIVMSG", channel, "Failed to open url: %s" % url )
-            server.send_event ( reply )
+            # The opening of URLs might fail
+            except IOError:
+                reply = irc.Irc_event ( "PRIVMSG", channel, "Failed to open url: %s" % url )
+                server.send_event ( reply )
 
 plugin_manager.register_event_handler ( "PRIVMSG", on_privmsg )
